5ee6c85f3b436b0740e06a735703e806645cf317
Aquib Ajani <aquib@tabdeel.in>
2022-08-12 19:28:51 +0530

    Add files via upload

---
+++ data_types and loops.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+int main()
+{
+    int choice;
+    //Data Types
+        
+        // primitive / built-in data types
+        
+        /*
+            int - whole numbers, positive, negative and 0 (4 bytes)
+            char - a-z, A-Z, 0-9, !@#$%^&*() (1 byte)
+            float - decimal number (4 bytes) 56.123456
+            double - decimal number (8 bytes)
+            void - empty
+        */
+        
+    //Loops
+    
+        //for
+        
+        /*
+            for(initilisation ; condition ; updation)
+            {
+                loop body
+            }
+        
+        
+            for(i=10; i>0; i--)
+            {
+                printf("%d\n", i);
+            }
+            
+        */
+        
+        //while
+        
+        /*
+            initilisation;
+            while(condition)
+            {
+                //loop body
+                //updation
+            }
+        */
+        
+       /*
+            i=1;
+            while(i<10)
+            {
+                printf("%d\n", i);
+                i++;
+            }
+        */
+        
+        //do-while
+        
+        /*
+            initilisation;
+            do{
+                //loop body
+                //updation
+            }while(condition);
+        */
+        
+        /* 
+            choice=0;
+            do{
+                printf("Here's the menu : 1. Pizza\n2.Pasta\n3. Noodles\n4. Tea\n5. Coffee\n6. Exit!");
+                scanf("%d", &choice);
+            }while(choice<6);
+        */
+    
+}
+
+
+
+
+
+
+
+
+
+
