cd9cd72d1583ac714081564598945e98addef247
Aquib Ajani <aquib@tabdeel.in>
2022-08-12 18:38:43 +0530

    Add files via upload

---
+++ decision_making.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+int main()
+{
+    
+    // int otp=123;
+    char grade = 'A';
+    // Decision Making
+        //ternary operator
+            //(condition) ? (code if true) : (code if false);
+            //otp==123 ? printf("Successful!") : printf("Incorrect!");
+        //if
+            // if(otp==123)
+            //     printf("Welcome back!");
+            
+        //if-else
+            /* if(otp==23)
+            {
+                printf("Welcome Back!");
+            }
+            else
+            {
+                printf("Incorrect!");
+            } */
+            
+        //nested if-else
+            /*
+            if(choice=="veg")
+            {
+                if(choice=="chinese")
+                {
+                    printf("Veg Chinese Menu : ");
+                }
+                else
+                {
+                    printf("Veg Non-Chinese Menu : ");
+                }
+            }
+            else
+            {
+                if(choice=="chinese")
+                {
+                    printf("Non-Veg Chinese Menu : ");
+                }
+                else
+                {
+                    printf("Non-Veg Non-Chinese Menu : ");
+                }
+            }
+            */
+        
+        //else-if ladder
+            /*
+                if(billAmount > 10000)
+                    printf("10% discount!");
+                else if(billAmount > 8000)
+                    printf("8% discount!");
+                else if(billAmount > 5000)
+                    printf("5% discount!");
+                else
+                    printf("No discount!");
+            */
+        
+        //Switch Case (fall through)
+        /* 
+            switch(grade)
+            {
+                case 'A' : printf("90 percent marks");
+                           break;
+                case 'B' : printf("80 percent marks");
+                           break;
+                case 'C' : printf("70 percent marks");
+                           break;
+                case 'D' : printf("60 percent marks");
+                           break;
+                case 'E' : printf("50 percent marks");
+                           break;
+                default : printf("Fail!");
+            }
+        */
+}
+
+
+
+
+
+
+
+
+
+
